dbfecc0763117794a23c8457df4b41ccac50880f
🧙‍♀️𝕿𝖍𝖊 𝖂𝖍𝖎𝖙𝖈𝖍™✨ <thewhitewitchtm@gmail.com>
2026-01-16 02:08:20 +0100

    Right Sidebar

--- src/app/aside-right.tsx
+++ src/app/aside-right.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
 import {Github, Mailbox, Twitter, Youtube} from "lucide-react";
+import {ThemeToggle} from "@/witchy/theme-toggle";
+import {Button} from "@/components/ui/button";
+import {Separator} from "@/components/ui/separator";
 
 export default function AsideRight() {
 	return (
-		<div>
-			<div className={"mb-4"}>
-				Links
-			</div>
-			<div className={"flex flex-col gap-3"}>
-				<Youtube/><Twitter/><Github/><Mailbox/>
-			</div>
+		<div className={"flex flex-col gap-2"}>
+				<ThemeToggle/>
+				<Separator/>
+				<Button variant={"ghost"} className={"h-auto p-0"}>
+					<Youtube/>
+				</Button>
+			<Button variant={"ghost"} className={"h-auto p-0"}>
+				<Twitter/>
+			</Button>
+				<Button variant={"ghost"} className={"h-auto p-0"}>
+					<Github/>
+				</Button>
+				<Separator/>
+				<Button variant={"ghost"} className={"h-auto p-0"}>
+					<Mailbox/>
+				</Button>
 		</div>
 	)
 }
